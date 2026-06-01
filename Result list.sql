2df627f00ba0da080063e8cc0ae08b9926d72580
Rayyan <Rayyanshafqat@gmail.com>
2026-06-01 21:15:12 +0500

    first commit

---
+++ Result list.sql
@@ -0,0 +1,21 @@
+Create TABLE ResultList (
+Name TEXT,
+Srno  INTEGER,
+Rollno  INTEGER
+);
+
+INSERT INTO ResultList (Name,Srno,Rollno) VALUES
+('Ali',01,188373),
+('Ahmed',02,188374),
+('Ahsan',03,188375),
+('Amir',04,188376),
+('Atif',05,188377),
+('Zafar',06,188378),
+('Hassan',07,188379),
+('Hashir',08,188380),
+('Musa',09,188381),
+('Haider',10,188383),
+('Arham',11,188385);
+
+SELECT*FROM ResultList;
+
---
+++ supplier table.sql
@@ -0,0 +1,15 @@
+Create table student(
+Name TEXT,
+Rno INTEGER,
+Class INTEGER,
+IDno INTEGER
+);
+
+INSERT INTO student (Name,Rno,Class,IDno) VALUES
+('Ali',188373,10,3455),
+('Ahmed',188372,10,3455),
+('Ayyan',188386,10,3455),
+('Ahsan',188425,10,3455),
+('Amir',188,10,3455);
+
+SELECT*FROM student;
