6e2ae2f5a50747297004b8f457ab478ab89cf1a7
Rayyan <Rayyanshafqat@gmail.com>
2026-06-03 21:19:05 +0500

    initial

---
+++ Employee.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Employees(
+Employee_ID INTEGER,
+Employee_name TEXT,
+Employee_salary INTEGER
+);
+
+INSERT INTO Employees(Employee_ID,Employee_name,Employee_salary) VALUES
+(01,'Hassan',100000),
+(02,'Hamza',50000),
+(03,'Ahsan',30000),
+(04,'Hashim',20000),
+(05,'Abas',10000);
+
+SELECT COUNT(Employee_ID)
+FROM Employees;
+
+SELECT AVG(Employee_salary)
+FROM Employees;
+
+SELECT SUM(Employee_salary)
+FROM Employees;
+
+SELECT*FROM Employees;
